88da30818446f18c604d73043af7019146963664
Kevin Rincon <kevinrincon8@gmail.com>
2023-11-10 17:24:23 -0500

    Correccion de bug en las vistas de cada rol

--- src/views/rol_views/DirectorView.jsx
+++ src/views/rol_views/DirectorView.jsx
@@ -13,12 +13,15 @@ export default function DirectorView() {
     return (
         <>
             <NavBar></NavBar>
-            <h1 className="container mx-auto my-10  text-3xl md:text-5xl text-with-600 font-bold leading-tight text-center md:text-left">Bienvenido, {user.name} </h1>
-            <div className="container mx-auto px-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
-                <CardItem cardTitle={"Funciones Sustantivas"} cardImg={TeachIcon} cardAltImg={"Icono de persona impartiendo clases"} cardLink={"/funciones-sustantivas"} />
-                <CardItem cardTitle={"Cursos"} cardImg={ClassIcon} cardAltImg={"Icono de tablero de clases"} cardLink={"/cursos"} />
-                <CardItem cardTitle={"Docentes"} cardImg={UsersIcon} cardAltImg={"Icono de usuarios"} cardLink={"/usuarios"} />
-                {/* Agregar Asignacion */}
+            <div className="container h-screen">
+
+                <h1 className="container mx-auto my-10  text-3xl md:text-5xl text-with-600 font-bold leading-tight text-center md:text-left">Bienvenido, {user.name} </h1>
+                <div className="container mx-auto px-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-10">
+                    <CardItem cardTitle={"Funciones Sustantivas"} cardImg={TeachIcon} cardAltImg={"Icono de persona impartiendo clases"} cardLink={"/funciones-sustantivas"} />
+                    <CardItem cardTitle={"Cursos"} cardImg={ClassIcon} cardAltImg={"Icono de tablero de clases"} cardLink={"/cursos"} />
+                    <CardItem cardTitle={"Docentes"} cardImg={UsersIcon} cardAltImg={"Icono de usuarios"} cardLink={"/usuarios"} />
+                    {/* Agregar Asignacion */}
+                </div>
             </div>
         </>
     )
