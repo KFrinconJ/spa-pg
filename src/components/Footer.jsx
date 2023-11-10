72c28087e523f3b2e1f9a8d7f88df630a826341e
Kevin Rincon <kevinrincon8@gmail.com>
2023-11-10 08:58:49 -0500

    Se arregla el footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,7 @@
 export default function Footer() {
     return (
-
-        <footer className="absolute bottom-0 inset-x-0 text-center py-5">
-            <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8">
+        <footer className="w-100 bottom-0 inset-x-0 text-center py-5">
+            <div className="mx-auto px-4 sm:px-6 lg:px-8">
                 <p className="text-sm text-white/50">Made by: <a className="text-white font-medium hover:text-white/80" href="https://github.com/KFrinconJ">KevDev</a></p>
             </div>
         </footer>
